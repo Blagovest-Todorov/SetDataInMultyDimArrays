2a11c0377f6677858894ec46fd4d96adecabcd6e
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-26 13:02:34 +0200

    entering a values for each row and col and then printing the Matrix elements

--- MultyArray.cs
+++ MultyArray.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Co0llections.Generic;
+using System.Collections.Generic;
 
 namespace _03.multyDimArrayTutorial
 {
@@ -7,17 +7,32 @@ namespace _03.multyDimArrayTutorial
     {
         static void Main(string[] args)
         {
-            int[,] numbers = new int[5, 5]; //we create array matrix of 2 rows and  2 columns
-            for (int row = 0; row < numbers.GetLength(0); row++)  //0 menas get lenght by row /
-            {
-                for (int col = 0; col < numbers.GetLength(1); col++) //get lenght 1 mean get lenght of the columns
-                {
-                    int value = int.Parse(Console.ReadLine());
-                    numbers[row, col] = value; //on curerent row columns it asignes the current value input;
+            Console.Write("Please enter number of rows ");
+            int rows = int.Parse(Console.ReadLine());
+            Console.Write("Please enter number of columns ");
+            int cols = int.Parse(Console.ReadLine());
+
+            int[,] numbers = new int[rows, cols];  //rows->the total number of the rows,cols->the total numebr of columns
+
+            for (int row = 0; row < rows; row++)  //numbers.GetLenght(0); --we take the size of rows
+            {                                       //row-> curretn row into the matrix
+                for (int col = 0; col < cols; col++) //numbers.GetLenght(1) --we take the lenght ,the size of columns
+                {                                     //col the current row into the Matrix 
+                    Console.Write("Enter Matrix[{0}, {1}]", row, col);
+                    numbers[row, col] = int.Parse(Console.ReadLine()); //opens a new console Line form  :
+                                                                       //where we read currect number Element that we set to a position currentRow and currentCol i.e (0,0)
 
                 }
-                //it is recommended to use lists in stead of oneDimensionalArrays;
-                //List<int> numbersList = new List<int>();
+            }
+            Console.WriteLine("your matrix is :");
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    //Console.WriteLine(numbers[row, col]);
+                    Console.Write("{0}", numbers[row, col]);
+                }
+                Console.WriteLine();
             }
 
         }
